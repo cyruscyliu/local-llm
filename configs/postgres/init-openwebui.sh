92649c8cf081b6aea6df333cc5f80f178f10ea93
Qiang Liu <cyruscyliu@gmail.com>
2026-03-06 11:50:14 +0100

    Fix Open WebUI DB init script

--- configs/postgres/init-openwebui.sh
+++ configs/postgres/init-openwebui.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<SQL
-DO $$
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" \
+  -v openwebui_user="$OPENWEBUI_DB_USER" \
+  -v openwebui_pass="$OPENWEBUI_DB_PASSWORD" \
+  -v openwebui_db="$OPENWEBUI_DB_NAME" <<'SQL'
+DO $do$
 BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = '${OPENWEBUI_DB_USER}') THEN
-    CREATE ROLE ${OPENWEBUI_DB_USER} LOGIN PASSWORD '${OPENWEBUI_DB_PASSWORD}';
+  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = :'openwebui_user') THEN
+    EXECUTE format('CREATE ROLE %I LOGIN PASSWORD %L', :'openwebui_user', :'openwebui_pass');
   END IF;
 END
-$$;
+$do$;
 
-DO $$
+DO $do$
 BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = '${OPENWEBUI_DB_NAME}') THEN
-    CREATE DATABASE ${OPENWEBUI_DB_NAME} OWNER ${OPENWEBUI_DB_USER};
+  IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = :'openwebui_db') THEN
+    EXECUTE format('CREATE DATABASE %I OWNER %I', :'openwebui_db', :'openwebui_user');
   END IF;
 END
-$$;
+$do$;
 SQL
